b58a3c9a5a4ce330527a0d234a173fe6459c0003
Ant1927 <ant1927dev@gmail.com>
2014-11-01 19:33:12 +0100

    reversible

--- Convertion en base 2.cpp
+++ Convertion en base 2.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
-using namespace std;
+using namespace std ;
 
-int main()
+void versBinaire(int nombreAconvertir)
 {
-    long long int nombreAconvertir(0);
-    cout << "Nombre a convertir : " ;
-    cin >> nombreAconvertir ;
-    string chaineFinal , chaineInter ;
-
+    string chaineFinal, chaineInter ;
     cout << "Le nombre en binaire s'ecrit : "  ;
     while (nombreAconvertir != 0)
     {
@@ -31,5 +28,42 @@ int main()
     }
     chaineFinal.erase(0,1) ;
     cout << chaineFinal << endl ;
+}
+
+void versDecimal(string nombreAconvertir)
+{
+    int taille(nombreAconvertir.size()),i(0) ;
+    int nombreConvertit(0) ;
+    for (i; i <=taille ; i++)
+    {
+        if (nombreAconvertir[i] == '1')
+        {
+
+        nombreConvertit += pow(2,taille-(i+1)) ;
+        }
+    }
+    cout << "Le nombre en ecriture decimal est : " << nombreConvertit ;
+}
+
+int main()
+{
+    int reponse(0) ;
+    cout << "En quoi voulez vous convertir le chiffre ? : " << endl ;
+    cout << "   Taper 1 pour binaire" << endl ;
+    cout << "   Taper 2 pour decimale" << endl ;
+    cin >> reponse ;
+    cout << "Entrez nombre : " ;
+    if (reponse ==1)
+    {
+        int nombre(0);
+        cin >> nombre ;
+        versBinaire(nombre) ;
+    }
+    else
+    {
+        string nombre ;
+        cin >> nombre ;
+        versDecimal(nombre) ;
+    }
     return 0 ;
 }
